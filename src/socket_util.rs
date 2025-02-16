f53ce7e10ac2d1ebf284d3d68e1f408a2049d5cf
cfal <cfal@users.noreply.github.com>
2025-02-16 05:48:13 +0400

    socket_util.rs: use socket2 directly, specify ipv6

--- src/socket_util.rs
+++ src/socket_util.rs
@@ -1,25 +1,68 @@
-#[inline]
-pub fn new_udp_socket(bind_interface: Option<String>) -> std::io::Result<tokio::net::UdpSocket> {
-    // TODO: this is blocking?
-    let std_socket = std::net::UdpSocket::bind("[::]:0")?;
-    std_socket.set_nonblocking(true)?;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::os::fd::{FromRawFd, IntoRawFd};
 
-    // tokio's UdpSocket has bind_device, so construct that instead of having to
-    // handle SO_BINDTODEVICE ourselves.
-    let tokio_socket = tokio::net::UdpSocket::from_std(std_socket).unwrap();
-    if let Some(_b) = bind_interface {
+use socket2::{Domain, Protocol, SockAddr, Socket, Type};
+
+pub fn new_udp_socket(
+    is_ipv6: bool,
+    bind_interface: Option<String>,
+) -> std::io::Result<tokio::net::UdpSocket> {
+    let socket =
+        new_socket2_udp_socket(is_ipv6, bind_interface, Some(get_sock_addr(is_ipv6)), false)?;
+
+    into_tokio_udp_socket(socket)
+}
+
+fn get_sock_addr(is_ipv6: bool) -> SockAddr {
+    let addr: std::net::SocketAddr = if !is_ipv6 {
+        SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 0)
+    } else {
+        "[::]:0".parse().unwrap()
+    };
+    SockAddr::from(addr)
+}
+
+fn new_socket2_udp_socket(
+    is_ipv6: bool,
+    bind_interface: Option<String>,
+    bind_address: Option<SockAddr>,
+    reuse_port: bool,
+) -> std::io::Result<socket2::Socket> {
+    let domain = if is_ipv6 { Domain::IPV6 } else { Domain::IPV4 };
+    let socket = Socket::new(domain, Type::DGRAM, Some(Protocol::UDP))?;
+
+    socket.set_nonblocking(true)?;
+
+    if reuse_port {
+        #[cfg(all(unix, not(any(target_os = "solaris", target_os = "illumos"))))]
+        socket.set_reuse_port(true)?;
+
+        #[cfg(any(not(unix), target_os = "solaris", target_os = "illumos"))]
+        panic!("Cannot support reuse sockets");
+    }
+
+    if let Some(ref interface) = bind_interface {
         #[cfg(any(target_os = "android", target_os = "fuchsia", target_os = "linux"))]
-        tokio_socket.bind_device(Some(_b.as_bytes()))?;
+        socket.bind_device(Some(interface.as_bytes()))?;
 
         // This should be handled during config validation.
         #[cfg(not(any(target_os = "android", target_os = "fuchsia", target_os = "linux")))]
         panic!("Could not bind to device, unsupported platform.")
     }
 
-    Ok(tokio_socket)
+    if let Some(ref bind_address) = bind_address {
+        socket.bind(bind_address)?;
+    }
+
+    Ok(socket)
 }
 
-#[inline]
+fn into_tokio_udp_socket(socket: socket2::Socket) -> std::io::Result<tokio::net::UdpSocket> {
+    let raw_fd = socket.into_raw_fd();
+    let std_udp_socket = unsafe { std::net::UdpSocket::from_raw_fd(raw_fd) };
+    Ok(tokio::net::UdpSocket::from_std(std_udp_socket)?)
+}
+
 pub fn new_tcp_socket(
     bind_interface: Option<String>,
     is_ipv6: bool,
