7ba7d73761f887fd60c440777ce852e7a91c60c4
cfal <cfal@users.noreply.github.com>
2025-02-17 07:11:47 +0400

    quic_stream.rs: reenable quinn poll_shutdown

--- src/quic_stream.rs
+++ src/quic_stream.rs
@@ -1,13 +1,14 @@
 use std::pin::Pin;
 use std::task::{Context, Poll};
 
+use quinn::{RecvStream, SendStream};
 use tokio::io::{AsyncRead, AsyncWrite, ReadBuf};
 
 use crate::async_stream::{AsyncPing, AsyncStream};
 
 pub struct QuicStream {
-    send_stream: quinn::SendStream,
-    recv_stream: quinn::RecvStream,
+    send_stream: SendStream,
+    recv_stream: RecvStream,
 }
 
 impl QuicStream {
@@ -21,33 +22,37 @@ impl QuicStream {
 
 impl AsyncRead for QuicStream {
     fn poll_read(
-        mut self: Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &mut ReadBuf<'_>,
     ) -> Poll<std::io::Result<()>> {
-        Pin::new(&mut self.recv_stream).poll_read(cx, buf)
+        let this = self.get_mut();
+        Pin::new(&mut this.recv_stream).poll_read(cx, buf)
     }
 }
 
 impl AsyncWrite for QuicStream {
     fn poll_write(
-        mut self: Pin<&mut Self>,
+        self: Pin<&mut Self>,
         cx: &mut Context<'_>,
         buf: &[u8],
     ) -> Poll<std::io::Result<usize>> {
-        Pin::new(&mut self.send_stream)
+        let this = self.get_mut();
+        Pin::new(&mut this.send_stream)
             .poll_write(cx, buf)
             .map_err(|err| err.into())
     }
 
-    fn poll_flush(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
-        Pin::new(&mut self.send_stream).poll_flush(cx)
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        let this = self.get_mut();
+        Pin::new(&mut this.send_stream).poll_flush(cx)
     }
 
-    fn poll_shutdown(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
-        // TODO: for some reason, this causes an unwrap and a mutex poison error in quinn.
-        // Pin::new(&mut self.send_stream).poll_shutdown(cx)
-        Poll::Ready(Ok(()))
+    fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<()>> {
+        // TODO: this was previously disabled because, because it caused a panic with a mutex poison error in quinn.
+        // ref: https://github.com/quinn-rs/quinn/issues/1298
+        let this = self.get_mut();
+        Pin::new(&mut this.send_stream).poll_shutdown(cx)
     }
 }
 
