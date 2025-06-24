5e1629959a031ad6c6511bef934b7798919adc61
cfal <cfal@users.noreply.github.com>
2025-06-24 15:15:35 +0900

    thread_util.rs: allow re-setting

--- src/thread_util.rs
+++ src/thread_util.rs
@@ -3,7 +3,7 @@ use std::sync::OnceLock;
 static NUM_THREADS: OnceLock<usize> = OnceLock::new();
 
 pub fn set_num_threads(num_threads: usize) {
-    NUM_THREADS.set(num_threads).unwrap();
+    let _ = NUM_THREADS.set(num_threads);
 }
 
 pub fn get_num_threads() -> usize {
